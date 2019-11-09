b5667ba1e2d18ee753940774fbb8a0001f3e1c54
diogo <diogo_p_nunes@hotmail.com>
2019-11-09 22:10:57 +0000

    encrypt with puk still doesnt work i think we need to use pem files

--- encryptor.py
+++ encryptor.py
@@ -1,8 +1,9 @@
-import os
-from Crypto.Cipher import PKCS1_OAEP
 from Cryptodome.Cipher import AES
-from Cryptodome.PublicKey import RSA
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_v1_5 as Cipher_PKCS1_v1_5
+from base64 import b64decode, b64encode
 from utils import *
+import os
 import utils
 
 
@@ -28,14 +29,21 @@ def decryptFile(filename, symmetric_key):
 
 
 def encryptSymmInMetadata(filename, symmetric_key, puk):
+    # remove this line when actual puk is sent
+    puk = """-----BEGIN PUBLIC KEY-----
+MIGfMA0GCSqGSIb3DQEBA3UAA4GNADCBiQKBgQC35eMaYoJXEoJt5HxarHkzDBEMU3qIWE0HSQ77CwP/8UbX07W2XKwngUyY4k6Hl2M/n9TOZMZsiBzer/fqV+QNPN1m9M94eUm2gQgwkoRj5battRCaNJK/23GGpCsTQatJN8PZBhJBb2Vlsvw5lFrSdMT1R7vaz+2EeNR/FitFXwIDAQAB
+-----END PUBLIC KEY-----"""
+
+    msg = "test"
     print(puk)
-    rsa_cipher = PKCS1_OAEP.new(RSA.importKey(puk))
-    data_encrypted = rsa_cipher.encrypt(symmetric_key)
+    keyPub = RSA.importKey(puk)
+    cipher = Cipher_PKCS1_v1_5.new(keyPub)
+    emsg = cipher.encrypt(msg.encode())
+    print(emsg)
+
     parts = filename.split("/")
     base = '/'.join(parts[:-1])
-    metadataFile = base+"/metadata."+parts[-1]
+    metadataFile = base + "/metadata." + parts[-1]
     print("[ENC] Created metadata file")
     with open(metadataFile, 'wb') as f:
-        f.write(data_encrypted)
-
-
+        f.write(cipher_text)
